9ba0c187bf04739259d1dd151028a6fe0bdbbd1d
Scott Yuan <scoworthyuan@gmail.com>
2022-12-11 10:46:02 +1100

    added simple testing unit

---
+++ __test__/validateForm.test.js
@@ -0,0 +1,7 @@
+import {validateForm} from "../src/client/js/formHandler"
+
+
+describe("Testing the validation functionality", () => {
+    test("Testing the validateForm() function", () => {
+        expect(validateForm).toEqual(undefined);
+    })});
--- src/client/js/validateForm.js
+++ src/client/js/validateForm.js
@@ -1,5 +1,16 @@
 function validateForm() {
-    
+    //check validaty of the content
+document.querySelector('.analyze').addEventListener('click',()=>{ 
+    let content = document.querySelector('.inputtext').value;
+    // let regexHttp = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/;
+    // let regexNoHttp = /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/;
+    if (content.length !=0){
+        console.log('true');
+    } else {
+        console.log('false');
+    }
+
+})
     
 }
 
