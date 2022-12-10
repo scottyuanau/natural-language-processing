7555f92ae35fa26b3193a2dc075ec48311472adf
Scott Yuan <scoworthyuan@gmail.com>
2022-12-11 10:45:45 +1100

    added jest for a simple testing unit.

--- src/client/index.js
+++ src/client/index.js
@@ -9,26 +9,5 @@ export {
 
 
 
-//check validaty of the url
-document.querySelector('.analyze').addEventListener('click',()=>{ 
-    let url = document.querySelector('.inputtext').value;
-    let regexHttp = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/;
-    let regexNoHttp = /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/;
-    if (url.match(regexHttp) || url.match(regexNoHttp)) {
-        console.log('true');
-    } else {
-        console.log('false');
-    }
 
-    fetchURLContent(url);
-})
 
-async function fetchURLContent(url){
-    let response = await fetch(url);
-    try {
-        console.log(response);
-        return response;
-    }  catch(error) {
-        console.log('error',error);
-    }
-};
